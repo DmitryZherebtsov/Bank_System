e9aa6f3345bb03a75cdca31a1ff059d068100866
Dmity <zherebtsovdima31@gmail.com>
2025-03-16 15:09:09 +0100

    launch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
 
+import { useState } from "react"
 
 const App = () => {
+
+  const [message, setMessagge] = useState("");
+
+  useState(() => {
+    fetch("http://127.0.0.1:8000/")
+      .then((response) => response.json())
+      .then((data) => setMessagge(data.message))
+      .catch((error) => console.error("Error fetching data:", error))
+  }, );
+
   return (
     <div>
-      MainApp
-      
+      <h1>Test Messagge</h1>
+      <p>{message}</p>
     </div>
   )
 }
